63cad8794dee392580a84499b1a4cc05c1aba251
Jene Scott <jscott@newrelic.com>
2018-04-08 00:17:29 -0700

    Completed Lesson 10

--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
@@ -14,10 +14,20 @@
  * limitations under the License.
  */
 
-// TODO (9) Create WaterReminderIntentService and extend it from IntentService
+// COMPLETED (9) Create WaterReminderIntentService and extend it from IntentService
+public class WaterReminderIntentServices extends IntentServices {
+    public WaterReminderIntentServices() {
+        super("WaterReminderIntentServices");
+    }
 
-//  TODO (10) Create a default constructor that calls super with the name of this class
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String action = intent.getAction();
+        ReminderTasks.executeTask(this, action);
+    }
+}
 
-//  TODO (11) Override onHandleIntent
-//      TODO (12) Get the action from the Intent that started this Service
-//      TODO (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+//  COMPLETED (10) Create a default constructor that calls super with the name of this class
+//  COMPLETED (11) Override onHandleIntent
+//      COMPLETED (12) Get the action from the Intent that started this Service
+//      COMPLETED (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
--- Lesson10-Hydration-Reminder/T10.04-Exercise-PeriodicSyncWithJobDispatcher/app/src/main/java/com/example/android/background/MainActivity.java
+++ Lesson10-Hydration-Reminder/T10.04-Exercise-PeriodicSyncWithJobDispatcher/app/src/main/java/com/example/android/background/MainActivity.java
@@ -53,8 +53,8 @@ public class MainActivity extends AppCompatActivity implements
         updateWaterCount();
         updateChargingReminderCount();
 
-        // TODO (23) Schedule the charging reminder
-
+        // COMPLETED (23) Schedule the charging reminder
+        ReminderUtilities.scheduleChargingReminder(this);
         /** Setup the shared preference listener **/
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         prefs.registerOnSharedPreferenceChangeListener(this);
@@ -93,11 +93,7 @@ public class MainActivity extends AppCompatActivity implements
     }
 
 
-    // TODO (24) Remove the button and testNotification code
-    
-    public void testNotification(View view) {
-        NotificationUtils.remindUserBecauseCharging(this);
-    }
+    // COMPLETED (24) Remove the button and testNotification code
 
     @Override
     protected void onDestroy() {
